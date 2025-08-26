d8a985744cf2a194503f68148ad1dd0ebacbd60d
HUSH2004-glitch <abdullahabidsaa004@gmail.com>
2025-08-26 23:35:31 +0500

    Added some methods in the likedlist

--- Linked_list/src/Main.java
+++ Linked_list/src/Main.java
@@ -1,19 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//My_linked_list list = new My_linked_list();
-//
-//list.add(10);
-//list.add(38);
-//list.add(20);
-//list.add(19);
-//list.display();
-//list.length();
-        My_linked_list temp = new My_linked_list();
-        temp.add_at_last(12);
-        temp.add_at_last(10);
-        temp.display();
-     
+        My_linked_list list = new My_linked_list();
+
+        list.add(20);
+        list.add(10);
+        list.insert_at(2, 15);
+        list.insert_at(0, 100);
+        list.display();
+        int temp = list.get_by_data(30);
+        list.remove_by_index(2);
     }
 }
--- Linked_list/src/My_linked_list.java
+++ Linked_list/src/My_linked_list.java
@@ -10,7 +10,7 @@ class node {
 }
 
 public class My_linked_list {
-    node head; // initially when the objec to fhte class is declared its value is by defualt null
+    node head; // initially, when the object to the class is declared, its value is by defualt null
 
     // method for addition of node
     void add(int data) {
@@ -30,8 +30,8 @@ public class My_linked_list {
         }
     }
 
-    //remove the linkedList
-    void remove(int data) {
+    //remove
+    void remove_by_data(int data) {
         if (head == null) return;
 
         if (head.data == data) {
@@ -47,10 +47,16 @@ public class My_linked_list {
         }
     }
 
+
     void add_first(int data) {
         node new_node = new node(data);
-        new_node.next = head;
-        head = new_node;
+        if (head == null) {// empty list
+            head = new_node;
+
+        } else {
+            new_node.next = head;
+            head = new_node;
+        }
     }
 
     void display() {
@@ -63,33 +69,70 @@ public class My_linked_list {
     }
 
     //for checking the length of the list
-    void length() {
+    int length() {
         node temp = head;
         int count = 0;
         while (temp != null) {
-            System.out.println(temp.data + " ");
             temp = temp.next;
             count++;
         }
-        System.out.println("Length : " + count);
+        return count;
     }
 
     // method for adding the function at the last
-    void add_at_last(int data){
+    void add_at_last(int data) {
         node new_node = new node(data);
 
-        if(head == null){
+        if (head == null) {
             head = new_node;
-        }
-        else {
+        } else {
             node temp = head;
-            while(temp.next != null){
+            while (temp.next != null) {
                 temp = temp.next;
             }
             temp.next = new_node;
         }
     }
 
+    //insert at particular index
+    void insert_at(int index, int data) {
+        node new_node = new node(data);
+        node temp = head;
+        if (index == 0) {
+            add_first(data);
+            return;
+        }
+        if (index == length()) {
+            add_at_last(data);
+            return;
+        }
+        for (int i = 0; i <= index - 1; i++) {
+            temp = temp.next;
+        }
+        new_node.next = temp.next;
+        temp.next = new_node;
+    }
 
+    int get_by_data(int data) {
+        node temp = head;
+        while (temp.next != null) {
+            if (temp.data == data) {
+                return data;
+            }
+            temp = temp.next;
+
+        }
+        return -1;
+    }
+
+    //fetching node by index;
+    int get_by_index(int index){
+        node temp = head;
+        for(int i=0 ;i<=index;i++){
+            temp = temp.next;
+
+        }
+        return temp.data;
+    }
 }
 
